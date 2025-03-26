647d3b5cb804fc183c6ee19bf8bc4619c88ee2b9
anant-jain01 <jain.anant609@gmail.com>
2025-03-26 17:20:01 +0530

    modified:   components/Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,7 +2,7 @@ export function Nav(){
   return(
     <header>
       <nav>
-        <img src="/react.png" />
+        <img src="https://www.pngwing.com/en/free-png-cgbgg" />
         <span>ReactFacts</span>
       </nav>
       </header>
