c86040e9f43df36c8a58d1e8a22021aa70661a45
anant-jain01 <jain.anant609@gmail.com>
2025-03-26 17:17:18 +0530

    modified:   components/Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,7 +2,7 @@ export function Nav(){
   return(
     <header>
       <nav>
-        <img src="components/react.png" />
+        <img src="./components/react.png" />
         <span>ReactFacts</span>
       </nav>
       </header>
